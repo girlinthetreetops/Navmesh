383658613425dbb2b2ab6b2973a49ac49e47706c
girlinthetreetops <andrea.stykket@gmail.com>
2024-02-08 11:36:20 +0000

    Lab 10.4

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,6 +10,17 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                agent.SetDestination(hit.point);
+            }
+        }
         
     }
 }
