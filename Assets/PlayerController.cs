ab1a7f53c69c8938732c6fad8286c0254bd7eb1f
girlinthetreetops <andrea.stykket@gmail.com>
2024-02-08 11:08:22 +0000

    Initial Commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera cam;
+    [SerializeField] private NavMeshAgent agent;
+
+    void Update()
+    {
+        
+    }
+}
